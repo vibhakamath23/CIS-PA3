d04dbf8317c486bb7379dc891b39651a3bf79ebe
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2025-11-11 16:30:05 -0500

    Refactor dk_from_sample_input for clarity and efficiency
    Refactor dk_from_sample_input function to improve clarity and efficiency by consolidating marker transformations and simplifying variable usage.

--- programs/dk_from_sample_input.m
+++ programs/dk_from_sample_input.m
@@ -1,35 +1,29 @@
 function [d_k] = dk_from_sample_input(samples, bodyA, bodyB)
-% Computes pointer tip positions and closest mesh points
-% Inputs:
+% compute pointer tip positions wrt to rigid body B frame
+% input:
 %   samples - struct containing split marker arrays
 %   bodyA, bodyB - structs with tip positions and marker locations
 %   mesh - surface mesh structure
-% Outputs:
+
+% Output:
 %   d_k - pointer tip positions in bone frame (N_samps x 3)
-%   c_k - closest points on mesh (N_samps x 3)
-%   diff_mag - distance between d_k and c_k (N_samps x 1)
 
-    N = samples.N_samps;
-    d_k = zeros(N, 3);
-
-    for k = 1:N
-        % Get LED marker positions for this frame
-        a_markers = samples.A_markers(:, :, k);  % N_A x 3
-        b_markers = samples.B_markers(:, :, k);  % N_B x 3
-
-        % Compute rigid body transformations
-        [~, ~, F_A] = point_cloud_registration(bodyA.markers, a_markers);
-        [~, ~, F_B] = point_cloud_registration(bodyB.markers, b_markers);
-
-        % Pointer tip position in tracker frame
-        A_tip_hom = [bodyA.tip'; 1];
-        A_tip_tracker = F_A * A_tip_hom;
-
-        % Transform to bone frame
-        F_B_inv = inv_homog(F_B);
-        d_k_hom = F_B_inv * A_tip_tracker;
-        d_k(k, :) = d_k_hom(1:3)';
-        
+    A_markers_body = bodyA.markers; % coords of markers A in body frame
+    B_markers_body = bodyB.markers; % coords of markers B in body frame
+    A_tip = bodyA.tip;
+    n_frames = samples.N_samps; 
+    d_k = zeros(n_frames, 3);
+    for i=1:n_frames
+        % compute frame transform F_A (from body A to tracker)
+        [~, ~, F_A] = point_cloud_registration(A_markers_body, ...
+                                                    samples.A_markers(:, :, i));
+        % compute frame transform F_B (from body B to tracker)
+        [~, ~, F_B] = point_cloud_registration(B_markers_body, ...
+                                                    samples.B_markers(:, :, i));
+        % compute frame transform from body A to body B
+        frame_transform_i = inv_homog(F_B) * F_A;
+        % get positions of pointer tip wrt body B
+        d_k(i, :) = transform_point(frame_transform_i, A_tip);
     end
 end
 
