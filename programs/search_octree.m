3c295341fe15230f3392eba7af77d9e6899a3d1b
Vibha Kamath <102431826+vibhakamath23@users.noreply.github.com>
2025-11-11 23:30:13 -0500

    Update search_octree.m

--- programs/search_octree.m
+++ programs/search_octree.m
@@ -59,20 +59,3 @@ function [point, min_dist] = search_octree(query, node, mesh)
     end
 end
 
-function dist = point_to_box_distance(point, box_min, box_max)
-% compute minimum distance from point to axis-aligned bounding box
-
-    % for each dimension, find closest point on box
-    closest = zeros(1, 3);
-    for i = 1:3
-        if point(i) < box_min(i)
-            closest(i) = box_min(i);
-        elseif point(i) > box_max(i)
-            closest(i) = box_max(i);
-        else
-            closest(i) = point(i);  % point is inside box in this dimension
-        end
-    end
-    
-    dist = norm(point - closest);
-end
