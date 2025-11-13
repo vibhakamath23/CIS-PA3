075d793b256c248dabf64bed881a19b782da0a23
Vibha Kamath <102431826+vibhakamath23@users.noreply.github.com>
2025-11-12 21:29:21 -0500

    Update create_random_body.m

--- tests/create_random_body.m
+++ tests/create_random_body.m
@@ -1,6 +1,5 @@
 function body = create_random_body(n_markers)
 % Generates random rigid body data for d_k unit tests 
-    % create random body definition
     body.markers = randn(n_markers, 3) * 10;
     body.tip = randn(1, 3) * 15;
     body.N_markers = n_markers;
